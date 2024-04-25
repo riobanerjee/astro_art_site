0ef3d7bb460dc37aeb29e1312d5c77e9e5dab45f
Agnibha Banerjee <riobanerjee@gmail.com>
2024-04-25 16:59:28 +0200

    Update Searchbar.tsx

--- frontend/src/components/Searchbar.tsx
+++ frontend/src/components/Searchbar.tsx
@@ -7,40 +7,29 @@ interface Props {
 }
 
 const SearchBar = ({apiRoute}: Props) => {
-
-  const [plansData, setPlansData] = useState({});
-
   const fetchData = () => {
     fetch('http://127.0.0.1:5000/search')
       .then(response => response.json())
       .then(data => {
         // Assuming the data is an array with the plans data at index 0 and the meals data at index 1
-        setMetaData(data);
+        setPlansData(data[0]);
+        setMealsData(data[1]);
       })
       .catch(error => console.error('Error fetching data:', error));
   };
-  
-  useEffect(() => {
-    fetch('http://127.0.0.1:5000/search')
-      .then(response => response.json())
-      .then(data => {
-        // Assuming the data is an array with the plans data at index 0 and the meals data at index 1
-        setMetaData(data);
-      })
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
   return (
     <div className="flex w-full max-w-sm items-center space-x-2">
       <Input type="query" placeholder="e.g. Galaxies" />
-      <Link to='/results'>
+
           <Button key="home-search" type="submit">
-            <button onClick={fetchData}>Go</button>
-    {/* Render your plans and meals or other UI elements here */}
-);
+      <button onClick={fetchData}>Go</button>
+
+            
+
           </Button>
-      </Link>
+      {/* Render your plans and meals or other UI elements here */}
     </div>
-  );
+);
 };
 
 export default SearchBar;
